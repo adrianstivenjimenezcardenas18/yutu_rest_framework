c7d39a734f6fd236151eb3b0bd44b1e31f1fe6e8
adrian stiven <jimenezcardenasad@gmail.com>
2021-11-09 16:54:38 -0500

    configuracion inicial

---
+++ ecommerce_rest/settings/production.py
@@ -0,0 +1,26 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure--=6d!0(3a@ri%lb*^*rjf*q^9eqhs5cnr29z^b@p0l^w6ie-9d'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
