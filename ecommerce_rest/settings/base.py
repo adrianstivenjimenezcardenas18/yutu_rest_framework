6c44e8b26b1944320d8264699c4b99fbde591b3d
adrian stiven <jimenezcardenasad@gmail.com>
2021-11-09 17:14:08 -0500

    haci queda la configuracion del  proyecto inicial

--- ecommerce_rest/settings/base.py
+++ ecommerce_rest/settings/base.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -26,6 +26,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+
+]
+
+THIRD_APPS = [
+    'rest_framework',
+]
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
